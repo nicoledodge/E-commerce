03de37d6b94015b9559b853412fa6f0999585ee6
nicoledodge <nicoledodge5@gmail.com>
2021-11-08 17:50:08 -0600

    finished product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,46 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
     // find all products
     // be sure to include its associated Category and Tag data
+    try{
+        const productData = await Product.findAll({
+            include: [{
+                model: Category,
+            },
+                {
+                    model: Tag,
+                }]
+        });
+        res.status(200).json(productData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
     // find a single product by its `id`
     // be sure to include its associated Category and Tag data
+    try {
+        const productData = await Product.findByPk(req.params.id, {
+            include: [{
+                model: Category,
+            },
+                {
+                    model: Tag,
+                }]
+        });
+        if (!productData){
+            res.status(404).json({
+                message: "No product found with this id!"
+            });
+        }
+        res.status(200).json(productData);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 // create new product
@@ -89,8 +120,23 @@ router.put('/:id', (req, res) => {
         });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
     // delete one product by its `id` value
+    try{
+        const deletedProduct = await Product.destroy(
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        );
+        if(!deletedProduct) {
+            res.status(404).json({message:"No product found with this id!"});
+        }
+        res.status(200).json(deletedProduct);
+    }catch (error) {
+        res.status(500).json(error);
+    }
 });
 
 module.exports = router;
